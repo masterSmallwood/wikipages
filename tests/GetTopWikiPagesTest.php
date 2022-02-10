70a9619d82d1a380fabe2f2cdf3b072e6031997f
owensmallwood <owen.smallwood@coconutsoftware.com>
2022-02-10 11:04:22 -0600

    Adds tests for generating top K domain pages

--- tests/GetTopWikiPagesTest.php
+++ tests/GetTopWikiPagesTest.php
@@ -11,17 +11,37 @@ class GetTopWikiPagesTest extends TestCase
         $pageViewsDownloader = Mockery::mock(PageViewDownloader::class);
         $pageViewsDownloader->shouldReceive('download')->andReturn('tests/wikiPageCounts');
 
-        $topPagesGenerator = new TopPagesGenerator($pageViewsDownloader, deniedDomains: [], topCount: 2);
+        $topPagesGenerator = new TopPagesGenerator($pageViewsDownloader, deniedPages: [], topCount: 2);
         $topPagesGenerator->generate('2022-02-01', 12);
 
         $content = file_get_contents('results/result_2022-02-01_12');
-        echo $content;
+        $rows = explode("\n", $content);
+
+        $this->assertEquals('subdomain1 page3 3', $rows[0]);
+        $this->assertEquals('subdomain1 page2 2', $rows[1]);
+        $this->assertEquals('subdomain2 page6 6', $rows[2]);
+        $this->assertEquals('subdomain2 page5 5', $rows[3]);
 
         unlink('results/result_2022-02-01_12');
     }
 
     public function testWillNotIncludePagesOnDomainDenyList()
     {
-        // TODO
+        $pageViewsDownloader = Mockery::mock(PageViewDownloader::class);
+        $pageViewsDownloader->shouldReceive('download')->andReturn('tests/wikiPageCounts');
+
+        $deniedPages = ['subdomain1' => ['page3' => true], 'subdomain2' => ['page6' => true]];
+        $topPagesGenerator = new TopPagesGenerator($pageViewsDownloader, deniedPages: $deniedPages, topCount: 2);
+        $topPagesGenerator->generate('2022-02-01', 12);
+
+        $content = file_get_contents('results/result_2022-02-01_12');
+        $rows = explode("\n", $content);
+
+        $this->assertEquals('subdomain1 page2 2', $rows[0]);
+        $this->assertEquals('subdomain1 page1 1', $rows[1]);
+        $this->assertEquals('subdomain2 page5 5', $rows[2]);
+        $this->assertEquals('subdomain2 page4 4', $rows[3]);
+
+        unlink('results/result_2022-02-01_12');
     }
 }
