79d85cc9461a0e70bf7f414725257142623a4f5e
owensmallwood <owen.smallwood@coconutsoftware.com>
2022-02-10 11:07:04 -0600

    Appends 0 on the hour when its a single digit

--- src/PageViewDownloader.php
+++ src/PageViewDownloader.php
@@ -15,13 +15,14 @@ class PageViewDownloader implements DownloadsFiles
      * @param string $hour 24-hour format. Enter value 0-23
      * @return string Return the filename
      */
-    public function download($date, $hour)
+    public function download($date, $hour) : string
     {
         $parsedDate = date_parse($date);
 
         $day = $parsedDate['day'] < 10 ? "0" . $parsedDate['day'] : (string)$parsedDate['day'];
         $month = $parsedDate['month'] < 10 ? "0" . $parsedDate['month'] : (string)$parsedDate['month'];
         $year = (string)$parsedDate['year'];
+        $hour = $hour < 10 ? "0" . $hour : $hour;
 
         $url = self::URL_BASE . "$year/$year-$month/pageviews-$year$month$day-$hour" . '0000.gz';
         $filename = "$year-$month-$day-$hour";
