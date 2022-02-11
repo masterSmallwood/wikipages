9caedc76ea0dc2c6e33949b9f61efef83f8f3e4a
owensmallwood <owen.smallwood@coconutsoftware.com>
2022-02-10 19:56:42 -0600

    fixes deprecation warning

--- src/PageViewsHeap.php
+++ src/PageViewsHeap.php
@@ -5,7 +5,7 @@ namespace App;
 use SplMinHeap;
 
 class PageViewsHeap extends SplMinHeap {
-    public function compare(mixed $value1, mixed $value2)
+    public function compare(mixed $value1, mixed $value2) : int
     {
         if ($value1[1] < $value2[1]) return 1;
         if ($value1[1] === $value2[1]) return 0;
